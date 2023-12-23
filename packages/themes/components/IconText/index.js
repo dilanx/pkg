495a8ebc2b2bd47a6a5f75bb4f157e991d22ad1e
Dilan <dilan@dilanxd.com>
2023-12-23 17:13:29 -0600

    themes: add IconText component

---
+++ packages/themes/components/IconText/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function IconText({ Icon, children }) {
+  return (
+    <span className={styles.container}>
+      <Icon className={styles.icon} />
+      <span>{children}</span>
+    </span>
+  );
+}
